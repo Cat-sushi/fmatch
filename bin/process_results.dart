2b08cb04a3c77feded433c8c979c5cfb171a25b9
Katsushi Yako <katsushi.yako@gmail.com>
2022-10-30 13:43:48 +0900

    Add pure q&a output

--- bin/process_results.dart
+++ bin/process_results.dart
@@ -6,11 +6,14 @@ Future<void> main(List<String> args) async {
   var trank = queryPath.substring(0, queryPath.lastIndexOf('.csv'));
   var resultsPath = '${trank}_results.csv';
   var forCompare = '${trank}_results_4c.csv';
+  var forCompare2 = '${trank}_results_4c2.csv';
   var forQueryStats = '${trank}_results_4q.csv';
   var sb = StringBuffer();
-  var f1 =
+  var fc =
       File(forCompare).openWrite()..add(utf8Bom);
-  var f2 =
+  var fc2 =
+      File(forCompare2).openWrite()..add(utf8Bom);
+  var fq =
       File(forQueryStats).openWrite()..add(utf8Bom);
   var lastIx = -1;
   await for (var l in readCsvLines(resultsPath)) {
@@ -18,15 +21,21 @@ Future<void> main(List<String> args) async {
     sb.write('${l[3]},${l[4]},${l[5]},${quoteCsvCell(l[6]!)},${quoteCsvCell(l[7]!)},${l[8]!}');
     l.sublist(9).map((e)=>',${quoteCsvCell(e!)}').forEach((e) => sb.write(e));
     sb.write('\r\n');
-    f1.write(sb);
+    fc.write(sb);
+    sb.clear();
+    sb.write('${quoteCsvCell(l[6]!)},${quoteCsvCell(l[7]!)},${l[8]!}');
+    l.sublist(9).map((e)=>',${quoteCsvCell(e!)}').forEach((e) => sb.write(e));
+    sb.write('\r\n');
+    fc2.write(sb);
     sb.clear();
     var ix = int.parse(l[2]!);
     if(ix == lastIx) {
       continue;
     }
     lastIx = ix;
-    f2.write('${l[0]},${l[1]},${l[2]},${l[3]},${l[4]},${l[5]},${quoteCsvCell(l[6]!)}\r\n');
+    fq.write('${l[0]},${l[1]},${l[2]},${l[3]},${l[4]},${l[5]},${quoteCsvCell(l[6]!)}\r\n');
   }
-  await f1.close();
-  await f2.close();
+  await fc.close();
+  await fc2.close();
+  await fq.close();
 }

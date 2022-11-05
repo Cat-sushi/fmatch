df2f2fc53f1213e940d8f95fa0e24401dcbaa250
Katsushi Yako <katsushi.yako@gmail.com>
2022-11-05 22:18:59 +0900

    Update result post processor

--- bin/process_results.dart
+++ bin/process_results.dart
@@ -9,31 +9,35 @@ Future<void> main(List<String> args) async {
   var forCompare2 = '${trank}_results_4c2.csv';
   var forQueryStats = '${trank}_results_4q.csv';
   var sb = StringBuffer();
-  var fc =
-      File(forCompare).openWrite()..add(utf8Bom);
-  var fc2 =
-      File(forCompare2).openWrite()..add(utf8Bom);
-  var fq =
-      File(forQueryStats).openWrite()..add(utf8Bom);
+  var fc = File(forCompare).openWrite()..add(utf8Bom);
+  var fc2 = File(forCompare2).openWrite()..add(utf8Bom);
+  var fq = File(forQueryStats).openWrite()..add(utf8Bom);
   var lastIx = -1;
   await for (var l in readCsvLines(resultsPath)) {
     sb.clear();
-    sb.write('${l[3]},${l[4]},${l[5]},${quoteCsvCell(l[6]!)},${quoteCsvCell(l[7]!)},${l[8]!}');
-    l.sublist(9).map((e)=>',${quoteCsvCell(e!)}').forEach((e) => sb.write(e));
+    sb.write(
+        '${l[3]},${l[4]},${l[5]},${quoteCsvCell(l[6]!)},${quoteCsvCell(l[7]!)},${l[8]!}');
+    l.sublist(9).map((e) => ',${quoteCsvCell(e!)}').forEach((e) => sb.write(e));
     sb.write('\r\n');
     fc.write(sb);
     sb.clear();
-    sb.write('${quoteCsvCell(l[6]!)},${quoteCsvCell(l[7]!)},${l[8]!}');
-    l.sublist(9).map((e)=>',${quoteCsvCell(e!)}').forEach((e) => sb.write(e));
-    sb.write('\r\n');
-    fc2.write(sb);
-    sb.clear();
+    if (l[7] != '') {
+      sb.write('${quoteCsvCell(l[6]!)},${quoteCsvCell(l[7]!)},${l[8]!}');
+      l
+          .sublist(9)
+          .map((e) => ',${quoteCsvCell(e!)}')
+          .forEach((e) => sb.write(e));
+      sb.write('\r\n');
+      fc2.write(sb);
+      sb.clear();
+    }
     var ix = int.parse(l[2]!);
-    if(ix == lastIx) {
+    if (ix == lastIx) {
       continue;
     }
     lastIx = ix;
-    fq.write('${l[0]},${l[1]},${l[2]},${l[3]},${l[4]},${l[5]},${quoteCsvCell(l[6]!)}\r\n');
+    fq.write(
+        '${l[0]},${l[1]},${l[2]},${l[3]},${l[4]},${l[5]},${quoteCsvCell(l[6]!)}\r\n');
   }
   await fc.close();
   await fc2.close();
